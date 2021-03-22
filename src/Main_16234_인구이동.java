e6ffa8bba3b59d269037d465bab51ba44f303eaa
esun1903 <dmstjs1996@naver.com>
2021-03-22 20:29:10 +0900

    0322

--- src/Main_16234_인구이동.java
+++ src/Main_16234_인구이동.java
@@ -6,99 +6,131 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_16234_인구이동 {
-
+	public static int result = 0; // 인구이동이 일어나는 횟수(결과값)
+	public static int dr[] = { -1, 1, 0, 0 };
+	public static int dc[] = { 0, 0, 1, -1 };
 	public static int N, L, R;
-	public static int map[][];
-	public static boolean check[][]; // 방문체크
-	public static int result = 0;
-	public static int peCheck = 1;
-	public static int[] dr = { -1, 1, 0, 0 };
-	public static int[] dc = { 0, 0, 1, -1 };
+	public static int visited[][];
+	public static int arr[][];
+	public static boolean check = false;
+	public static int count = 0;
 
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
 
-		N = Integer.parseInt(st.nextToken()); // N*N의 땅이 있는 것
-		L = Integer.parseInt(st.nextToken()); // L명 이상
-		R = Integer.parseInt(st.nextToken()); // R명 이하
-       
-		map = new int[N][N];
+		arr = new int[N][N];
+		visited = new int[N][N];
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				visited[i][j] = 0;
 			}
-		} // 입력받기
+		}
 
-           	 for (int i = 0; i < N; i++) {
-					for (int j = 0; j < N; j++) {
-						     BFS(i,j);
-					}
-				}
-           
+		while (true) {
+			visited = new int[N][N];
 
-		System.out.println(result);
-
-	}
-
-	private static void BFS(int x, int y) {
-		Queue<Pair> queue = new LinkedList<Pair>();
-		queue.offer(new Pair(0, 0));
-		peCheck = 1;
-		int sum = map[x][y];
-		while (!queue.isEmpty()) {
-			Pair p = queue.poll();
-
-			for (int i = 0; i < 4; i++) {
-				int nr = p.r + dr[i];
-				int nc = p.c + dc[i];
-				if (0 <= nr && nr < N && 0 <= nc && nc < N && !check[nr][nc]) { // 만약 범위 안에 존재하고 방문하지 않았다면 들어가기
-					int sub = Math.abs(map[nr][nc] - map[p.r][p.c]); // 만약 내가 갈 곳의 위치 - 지금 위치 뺀게
-					if (L <= sub && R >= sub) { // L보단 크거나 같고 R보단 작거나 같다면
-						queue.add(new Pair(nr, nc)); // 큐에 넣는다.
-						sum += map[nr][nc];
-						peCheck++;
-						check[nr][nc] = true;
-						if (peCheck == N * N && result == 0) {
-							System.out.println(result + 1);
-							System.exit(0);
-						}
-					}
-				}
-			}
-		} // end of while
-
-		if (peCheck > 1) {
-			int cal = sum / peCheck;
-			result++;
+			int now = 0;
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					if (check[i][j] == true) { // 만약에 true이면
-						map[i][j] = cal; // 변경
+					if (visited[i][j] == 0) { // 아직 안되어있는 것들만
+						now += BFS(i, j);
+
+//						System.out.println("#####################");
+//						for (int r = 0; r < N; r++) {
+//							for (int c = 0; c < N; c++) {
+//								System.out.print(arr[r][c] + " ");
+//							}
+//							System.out.println();
+//						}
+//						System.out.println("#####################");
+					}
+				}
+			}
+
+//			System.out.println("#####################");
+//			for (int r = 0; r < N; r++) {
+//				for (int c = 0; c < N; c++) {
+//					System.out.print(arr[r][c] + " ");
+//				}
+//				System.out.println();
+//			}
+//			System.out.println("#####################");
+
+			// 인구 이동이 없을 때까지 지속된다.
+			if (now > 0) { // 만약, 인구 이동이 있다면 ++;
+				result++;
+			} else { // 만약, 인구 이동이 없다면 false
+				break;
+			}
+		}
+
+		System.out.println(result);
+	}
+
+	private static int BFS(int i, int j) {
+		count++;
+		Queue<Pair> queue = new LinkedList<Pair>();
+		queue.add(new Pair(i, j));
+		int 인구수 = 1;
+		int sum = arr[i][j];
+		visited[i][j] = count;
+		while (!queue.isEmpty()) {
+			Pair p = queue.poll();
+			for (int d = 0; d < 4; d++) {
+				int nr = p.r + dr[d];
+				int nc = p.c + dc[d];
+
+				// L명 이상, R명 이하라면, 두 나라가 공유하는 국경선을 오늘 하루동안 연다. , 방문을 안했거나
+				if (0 <= nr && 0 <= nc && nr < N && nc < N && visited[nr][nc] == 0) {
+					int sub = Math.abs(arr[p.r][p.c] - arr[nr][nc]);
+					if (L <= sub && R >= sub) {
+						queue.offer(new Pair(nr, nc));
+						visited[nr][nc] = count;
+						if (인구수 == 1) {
+//							result++;
+						}
+						인구수++;
+						sum += arr[nr][nc];
 					}
 				}
 			}
 		}
-		for (int i = 0; i < N; i++) {
-			System.out.println();
-			for (int j = 0; j < N; j++) {
-				System.out.print(map[i][j] + " ");
+
+//		System.out.println("=============");
+//		for (int k = 0; k < N; k++) {
+//			for (int k2 = 0; k2 < N; k2++) {
+//				System.out.print(visited[k][k2] + " ");
+//			}
+//			System.out.println();
+//		}
+
+//		System.out.println(인구수);
+		if (인구수 > 1) { // 연합을 이루고 있는 각 칸의 인구수는 (연합의 인구수) / (연합을 이루고 있는 칸의 개수)가 된다. 편의상 소수점은 버린다.
+			int m = (sum) / (인구수);
+			check = true;
+			for (int r = 0; r < N; r++) {
+				for (int c = 0; c < N; c++) {
+					if (visited[r][c] == count) {
+						arr[r][c] = m;
+					}
+				}
 			}
-		} // 입력받기
-       System.out.println();
-		for (int i = 0; i < N; i++) {
-			System.out.println();
-			for (int j = 0; j < N; j++) {
-				if(check[i][j] == true)
-					System.out.print(1+" ");
-				else
-					System.out.print(0+" ");
-			}
-		} // 입력받기
-       System.out.println();
+		} else {
+			check = false;
+		}
+
+		if (인구수 == 1)
+			인구수--;
+
+		return 인구수;
 
 	}
 
@@ -129,8 +161,4 @@ public class Main_16234_인구이동 {
  * 주어진다. r행 c열에 주어지는 정수는 A[r][c]의 값이다 인구 이동이 발생하는 횟수가 2,000번 보다 작거나 같은 입력만 주어진다
  * 
  * 출력 : 인구 이동이 몇 번 발생하는지 첫째 줄에 출력한다.
- * 
- * 
- * for (int r = 0; r < N; r++) { System.out.println(); for (int c = 0; c < N;
- * c++) { System.out.print(check[r][c] + " "); } } // 입력받기
  */
