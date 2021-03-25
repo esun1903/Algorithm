76ded17f07980a2b36991d727c8142c974f20c44
esun1903 <dmstjs1996@naver.com>
2021-03-26 00:26:16 +0900

    test

--- src/BinaryCountingTest.java
+++ src/BinaryCountingTest.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class BinaryCountingTest {
  
 	static int N; 
-	static int [] numbers; 
+	static int [] numbers;  
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
